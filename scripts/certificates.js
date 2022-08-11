d400c85616a200f6bba3952b1bf4f08436798e42
Ruan Oliveira <ruanamaral40@gmail.com>
2022-08-11 16:03:57 -0300

    Parte quase finalizada

--- scripts/certificates.js
+++ scripts/certificates.js
@@ -1,17 +1,72 @@
-const botao_clone = document.querySelector('.form__button--more')
+const botaoMore = document.querySelector('.form__button--more');
+const formItem = document.querySelector('#certificates');
 
-botao_clone.addEventListener('click', clone => {
+let arrayFavorito = [];
+let arrayAntigo = [];
 
-    clone.preventDefault();
+botaoMore.addEventListener('click', moreCertificates);
+formItem.addEventListener('click', FavoriteCertificates);
 
-    const inputCollection = document.querySelectorAll('.certificates__input');
+function FavoriteCertificates(event) {
+
+    const heartElement = document.querySelectorAll('.favorite__icon');
+
+    for (let i = 0; i < heartElement.length; i++) {
+
+        if (event.target == heartElement[i]) {
+
+            const divFavorite = event.target.parentNode;
+
+            if (arrayFavorito.indexOf(divFavorite) == -1) {
+                arrayFavorito.push(divFavorite);
+                divFavorite.remove();  
+            }
+            renderDiv();
+        }
+    }
+}
+
+function renderDiv() {
+
+    let lastItem = [...arrayFavorito].pop();
+
+    formItem.innerHTML += `
+        <div class='favorite__input favorite__input--active'>
+            ${lastItem.innerHTML}
+        </div>
+    `
+}
+
+function renderRemoveFavorite(div) {
+    const favoriteDiv = document.querySelectorAll('.favorite__input');
+
+    div.classList.remove('favorite__input--active');
+}
+
+function moreCertificates(event) {
+
+    event.preventDefault();
+
+    const inputCollection = document.querySelectorAll('.favorite__input');
 
     if (inputCollection.length <= 4) {
-        const inputElement = document.createElement('input');
 
+        const divElement = document.createElement('div');
+        divElement.classList.add('favorite__input');
+        document.querySelector('#certificates').appendChild(divElement);
+
+        const inputElement = document.createElement('input');
+        inputElement.placeholder = 'favo';
         inputElement.classList.add('certificates__input');
 
-        document.querySelector('#certificates').appendChild(inputElement);
-    }
+        const spanElement = document.createElement('span');
+        spanElement.classList.add('favorite__icon');
 
-})
+        const inputCollection = document.querySelectorAll('.favorite__input');
+
+        let lastElement = [...inputCollection].pop();
+
+        lastElement.appendChild(inputElement);
+        lastElement.appendChild(spanElement);
+    }
+}
